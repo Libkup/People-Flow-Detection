d3dd485bfdfb6fa432c2bb8f691afa3694c70013
dtid_abcc77f3f2caa71b <yushang_jg@163.com>
2019-07-02 17:56:09 +0800

    entity Cameralog

---
+++ src/entity/Cameralog.java
@@ -0,0 +1,46 @@
+package entity;
+
+import java.sql.Timestamp;
+
+public class Cameralog {
+	private int id;
+	private Timestamp time;
+	private int count;
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the time
+	 */
+	public Timestamp getTime() {
+		return time;
+	}
+	/**
+	 * @param time the time to set
+	 */
+	public void setTime(Timestamp time) {
+		this.time = time;
+	}
+	/**
+	 * @return the count
+	 */
+	public int getCount() {
+		return count;
+	}
+	/**
+	 * @param count the count to set
+	 */
+	public void setCount(int count) {
+		this.count = count;
+	}
+}
