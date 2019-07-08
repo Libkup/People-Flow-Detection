ccb2d91ed9a4c544d5bd73669d8a556c4a071053
npu_JG <yushang_jg@163.com>
2019-07-08 15:31:48 +0800

    管理员登出功能

---
+++ src/controller/admin/AdminLogout.java
@@ -0,0 +1,46 @@
+package controller.admin;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdminLogout
+ */
+@WebServlet("/AdminLogout")
+public class AdminLogout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminLogout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		if (request.getSession(false) != null && request.getSession().getAttribute("loginAdminName") != null) {
+			// used to clear the session
+			request.getSession().invalidate();
+		}
+		response.sendRedirect("adminlogin.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
